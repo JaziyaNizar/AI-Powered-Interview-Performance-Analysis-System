33be57d275712a8429f491f7b1d9a2830aad8d6b
Jaziya Nizar <jaziyanizar321@gmail.com>
2026-08-11 14:16:24 +0530

    Update emotion_test.py

--- emotion_test.py
+++ emotion_test.py
@@ -59,35 +59,5 @@ def detect_emotion(frame):
 
 
 
-# MAIN PROGRAM
-if __name__ == "__main__":
 
-    cap = cv2.VideoCapture(0)
 
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        emotion = detect_emotion(frame)
-
-        # Display emotion text
-        cv2.putText(
-            frame,
-            f"Emotion: {emotion}",
-            (30, 50),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255, 0, 0),
-            2
-        )
-
-        cv2.imshow("Emotion Detection", frame)
-
-        # Press q to quit
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
